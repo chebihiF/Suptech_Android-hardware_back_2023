7c222882ba61277b41b89d563ed26409361af89c
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-13 12:01:02 +0100

    Initial commit

---
+++ src/main/java/org/suptech/hardware/HardwareBackApplication.java
@@ -0,0 +1,13 @@
+package org.suptech.hardware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareBackApplication.class, args);
+	}
+
+}
